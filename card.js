e1040e229ace903c200956feeb2df0738c2bdaa3
Fátima <fatimaalvesa10@gmail.com>
2024-03-03 17:50:10 +0000

    Started fixing the dropdown

--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-const Card = (
+const Card = ({
   image,
   type,
   name,
@@ -11,8 +11,8 @@ const Card = (
   height,
   weight,
   ability,
-  hiddenAbility
-) => {
+  hiddenAbility,
+}) => {
   const typesMap = {
     FIRE: "card-fire",
     WATER: "card-water",
@@ -44,125 +44,185 @@ const Card = (
   const cardStats = document.createElement("div");
   cardStats.className = "card-stats";
   card.appendChild(cardStats);
-  
 
   const cardtype = document.createElement("p");
   cardtype.className = "card-type";
   cardtype.innerHTML = type;
   cardStats.appendChild(cardtype);
-  
+
   const cardName = document.createElement("h2");
   cardName.className = "card-name";
   cardName.innerHTML = name;
   cardStats.appendChild(cardName);
-  
-  ////////////////////////////////HP////////////////////////////////
-  const cardhp = document.createElement("p");
-  cardhp.innerHTML = `HP: `;
-  cardStats.appendChild(cardhp);
 
-  const hpValue = document.createElement("span");
+  ////////////////////////////////HP////////////////////////////////
+  const cardhp = document.createElement("div");
+  cardhp.className = "stat-container"; 
+  
+  const hpLabel = document.createElement("p");
+  hpLabel.className = "stat-label";
+  hpLabel.innerHTML = `HP: `;
+  cardhp.appendChild(hpLabel);
+  
+  const hpValue = document.createElement("p");
+  hpValue.className = "stat-value";
   hpValue.innerHTML = hp;
   cardhp.appendChild(hpValue);
+  
+  cardStats.appendChild(cardhp);
+///////////////////////////////ATTACK////////////////////////////////
+const cardAttacks = document.createElement("div");
+cardAttacks.className = "stat-container";
 
-  ////////////////////////////////ATTACK////////////////////////////////
-  const cardAttacks = document.createElement("p");
-  cardAttacks.innerHTML = `ATTACK: `;
-  cardStats.appendChild(cardAttacks);
+const cardAttacksLabel = document.createElement("p");
+cardAttacksLabel.className = "stat-label";
+cardAttacksLabel.innerHTML = `ATTACK: `;
+cardAttacks.appendChild(cardAttacksLabel);
 
-  const cardAttacksValue = document.createElement("span");
-  cardAttacksValue.innerHTML = attacks;
-  cardAttacks.appendChild(cardAttacksValue);
+const cardAttacksValue = document.createElement("p");
+cardAttacksValue.className = "stat-value";
+cardAttacksValue.innerHTML = attacks;
+cardAttacks.appendChild(cardAttacksValue);
 
-  ////////////////////////////////DEFENSE////////////////////////////////
-  const cardDefense = document.createElement("p");
-  cardDefense.innerHTML = `DEFENSE: `;
-  cardStats.appendChild(cardDefense);
+cardStats.appendChild(cardAttacks);
 
-  const cardDefenseValue = document.createElement("span");
-  cardDefenseValue.innerHTML = defense;
-  cardDefense.appendChild(cardDefenseValue);
+///////////////////////////////DEFENSE////////////////////////////////
+const cardDefense = document.createElement("div");
+cardDefense.className = "stat-container";
 
-  ////////////////////////////////SPECIAL ATTACK////////////////////////////////
-  const cardspecialAttack = document.createElement("p");
-  cardspecialAttack.innerHTML = `SPECIAL ATTACK: `;
-  cardStats.appendChild(cardspecialAttack);
+const cardDefenseLabel = document.createElement("p");
+cardDefenseLabel.className = "stat-label";
+cardDefenseLabel.innerHTML = `DEFENSE: `;
+cardDefense.appendChild(cardDefenseLabel);
 
-  const cardspecialAttackValue = document.createElement("span");
-  cardspecialAttackValue.innerHTML = specialAttack;
-  cardspecialAttack.appendChild(cardspecialAttackValue);
+const cardDefenseValue = document.createElement("p");
+cardDefenseValue.className = "stat-value";
+cardDefenseValue.innerHTML = defense;
+cardDefense.appendChild(cardDefenseValue);
 
-  ////////////////////////////////SPECIAL DEFENSE ////////////////////////////////
-  const cardspecialDefense = document.createElement("p");
-  cardspecialDefense.innerHTML = `SPECIAL DEFENSE: `;
-  cardStats.appendChild(cardspecialDefense);
+cardStats.appendChild(cardDefense);
 
-  const cardspecialDefenseValue = document.createElement("span");
-  cardspecialDefenseValue.innerHTML = specialDefense;
-  cardspecialDefense.appendChild(cardspecialDefenseValue);
+///////////////////////////////SPECIAL ATTACK////////////////////////////////
+const cardspecialAttack = document.createElement("div");
+cardspecialAttack.className = "stat-container";
 
-  ////////////////////////////////SPEED ////////////////////////////////
-  const cardspeed = document.createElement("p");
-  cardspeed.innerHTML = `SPEED: `;
-  cardStats.appendChild(cardspeed);
+const cardspecialAttackLabel = document.createElement("p");
+cardspecialAttackLabel.className = "stat-label";
+cardspecialAttackLabel.innerHTML = `SPECIAL ATTACK: `;
+cardspecialAttack.appendChild(cardspecialAttackLabel);
 
-  const cardspeedValue = document.createElement("span");
-  cardspeedValue.innerHTML = speed;
-  cardspeed.appendChild(cardspeedValue);
+const cardspecialAttackValue = document.createElement("p");
+cardspecialAttackValue.className = "stat-value";
+cardspecialAttackValue.innerHTML = specialAttack;
+cardspecialAttack.appendChild(cardspecialAttackValue);
 
-  ////////////////////////////////HEIGHT ////////////////////////////////
-  const cardheight = document.createElement("p");
-  cardheight.innerHTML = `HEIGHT: `;
-  cardStats.appendChild(cardheight);
+cardStats.appendChild(cardspecialAttack);
 
-  const cardheightValue = document.createElement("span");
-  cardheightValue.innerHTML = height;
-  cardheight.appendChild(cardheightValue);
+///////////////////////////////SPECIAL DEFENSE ////////////////////////////////
+const cardspecialDefense = document.createElement("div");
+cardspecialDefense.className = "stat-container";
 
-  ////////////////////////////////WEIGHT ////////////////////////////////
-  const cardweight = document.createElement("p");
-  cardweight.innerHTML = `WEIGHT: `;
-  cardStats.appendChild(cardweight);
+const cardspecialDefenseLabel = document.createElement("p");
+cardspecialDefenseLabel.className = "stat-label";
+cardspecialDefenseLabel.innerHTML = `SPECIAL DEFENSE: `;
+cardspecialDefense.appendChild(cardspecialDefenseLabel);
 
-  const cardweightValue = document.createElement("span");
-  cardweightValue.innerHTML = weight;
-  cardweight.appendChild(cardweightValue);
+const cardspecialDefenseValue = document.createElement("p");
+cardspecialDefenseValue.className = "stat-value";
+cardspecialDefenseValue.innerHTML = specialDefense;
+cardspecialDefense.appendChild(cardspecialDefenseValue);
 
-////////////////////////////////ABILITY SECTION////////////////////////////////
+cardStats.appendChild(cardspecialDefense);
+
+///////////////////////////////SPEED ////////////////////////////////
+const cardspeed = document.createElement("div");
+cardspeed.className = "stat-container";
+
+const cardspeedLabel = document.createElement("p");
+cardspeedLabel.className = "stat-label";
+cardspeedLabel.innerHTML = `SPEED: `;
+cardspeed.appendChild(cardspeedLabel);
+
+const cardspeedValue = document.createElement("p");
+cardspeedValue.className = "stat-value";
+cardspeedValue.innerHTML = speed;
+cardspeed.appendChild(cardspeedValue);
+
+cardStats.appendChild(cardspeed);
+
+///////////////////////////////HEIGHT ////////////////////////////////
+const cardheight = document.createElement("div");
+cardheight.className = "stat-container";
+
+const cardheightLabel = document.createElement("p");
+cardheightLabel.className = "stat-label";
+cardheightLabel.innerHTML = `HEIGHT: `;
+cardheight.appendChild(cardheightLabel);
+
+const cardheightValue = document.createElement("p");
+cardheightValue.className = "stat-value";
+cardheightValue.innerHTML = height;
+cardheight.appendChild(cardheightValue);
+
+cardStats.appendChild(cardheight);
+
+///////////////////////////////WEIGHT ////////////////////////////////
+const cardweight = document.createElement("div");
+cardweight.className = "stat-container";
+
+const cardweightLabel = document.createElement("p");
+cardweightLabel.className = "stat-label";
+cardweightLabel.innerHTML = `WEIGHT: `;
+cardweight.appendChild(cardweightLabel);
+
+const cardweightValue = document.createElement("p");
+cardweightValue.className = "stat-value";
+cardweightValue.innerHTML = weight;
+cardweight.appendChild(cardweightValue);
+
+cardStats.appendChild(cardweight);
+
+///////////////////////////////ABILITY SECTION////////////////////////////////
 const cardAbilityContainer = document.createElement("div");
-cardStats.appendChild(cardAbilityContainer);
+cardAbilityContainer.className = "stat-container";
 
 const abilitiesDiv = document.createElement("div");
 abilitiesDiv.className = "ABILITIES";
 cardAbilityContainer.appendChild(abilitiesDiv);
 
-///////////////////////////////NORMAL ABILITY SECTION////////////////////////////////
+//////////////////////////////NORMAL ABILITY SECTION////////////////////////////////
 const cardAbilityDiv = document.createElement("div");
-abilitiesDiv.appendChild(cardAbilityDiv);
 
 const cardAbility = document.createElement("p");
+cardAbility.className = "stat-label";
 cardAbility.innerHTML = `ABILITY: `;
 cardAbilityDiv.appendChild(cardAbility);
 
 const cardAbilityValue = document.createElement("p");
+cardAbilityValue.className = "stat-value";
 cardAbilityValue.innerHTML = ability;
-cardAbility.appendChild(cardAbilityValue);
+cardAbilityDiv.appendChild(cardAbilityValue);
 
-///////////////////////////////HIDDEN ABILITY SECTION////////////////////////////////
+abilitiesDiv.appendChild(cardAbilityDiv);
+
+//////////////////////////////HIDDEN ABILITY SECTION////////////////////////////////
 const cardHiddenAbilityDiv = document.createElement("div");
-abilitiesDiv.appendChild(cardHiddenAbilityDiv);
 
 const cardHiddenAbility = document.createElement("p");
+cardHiddenAbility.className = "stat-label";
 cardHiddenAbility.innerHTML = `HIDDEN ABILITY: `;
 cardHiddenAbilityDiv.appendChild(cardHiddenAbility);
 
 const cardHiddenAbilityValue = document.createElement("p");
+cardHiddenAbilityValue.className = "stat-value";
 cardHiddenAbilityValue.innerHTML = hiddenAbility;
-cardHiddenAbility.appendChild(cardHiddenAbilityValue);
-  
+cardHiddenAbilityDiv.appendChild(cardHiddenAbilityValue);
+
+abilitiesDiv.appendChild(cardHiddenAbilityDiv);
+
+cardStats.appendChild(cardAbilityContainer);
+
 cardsContainer.appendChild(card);
 };
 
 export default Card;
-
-
--- cardContainer.js
+++ cardContainer.js
@@ -1,26 +1,43 @@
 import Card from "/card.js";
+
 document.addEventListener("DOMContentLoaded", function () {
   fetch("pokemons.json")
     .then((response) => response.json())
-    .then((data) => renderPokemonCards(data.pokemons));
-});
+    .then((data) => {
+      const dropdown = document.createElement("div");
+      dropdown.className = "dropdown";
+      dropdown.innerHTML = data.pokemons[0].name;
 
-function renderPokemonCards(pokemons) {
-  pokemons.forEach((pokemon) => {
-    Card(
-      pokemon.image,
-      pokemon.type,
-      pokemon.name,
-      pokemon.hp,
-      pokemon.attacks,
-      pokemon.defense,
-      pokemon.specialAttack,
-      pokemon.specialDefense,
-      pokemon.speed,
-      pokemon.height,
-      pokemon.weight,
-      pokemon.ability,
-      pokemon.hiddenAbility
-    );
-  });
-}
+      const dropdownContent = document.createElement("div");
+      dropdownContent.className = "dropdown-content";
+
+      data.pokemons.forEach((pokemon) => {
+        const option = document.createElement("div");
+        option.className = "option";
+        option.innerHTML = pokemon.name;
+        option.addEventListener("click", function () {
+          dropdown.innerHTML = this.innerHTML;
+          const selectedPokemon = data.pokemons.find(
+            (pokemon) => pokemon.name === this.innerHTML
+          );
+
+          const cardsContainer = document.getElementById("my-cards");
+          cardsContainer.innerHTML = "";
+          Card(selectedPokemon);
+        });
+        dropdownContent.appendChild(option);
+      });
+
+      dropdown.appendChild(dropdownContent);
+      document.body.insertBefore(dropdown, document.getElementById("my-cards")); // faz ficar no topo
+      Card(data.pokemons[0]);
+    });
+
+    dropdownContent.addEventListener("click", function () {
+      if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+      } else {
+        dropdownContent.style.display = 'block';
+      }
+    });
+});
